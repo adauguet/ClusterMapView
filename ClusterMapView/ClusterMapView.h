3285f01a4ba8a65e3a1d3ee5e6f4ee684e588352
Antoine DAUGUET <antoine.dauguet@schibsted.com>
2017-05-04 18:12:03 +0200

    Initial Commit

---
+++ ClusterMapView/ClusterMapView.h
@@ -0,0 +1,19 @@
+//
+//  ClusterMapView.h
+//  ClusterMapView
+//
+//  Created by Antoine DAUGUET on 04/05/2017.
+//  Copyright © 2017 Antoine DAUGUET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ClusterMapView.
+FOUNDATION_EXPORT double ClusterMapViewVersionNumber;
+
+//! Project version string for ClusterMapView.
+FOUNDATION_EXPORT const unsigned char ClusterMapViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClusterMapView/PublicHeader.h>
+
+
